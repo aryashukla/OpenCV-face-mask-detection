2ccda801f1656bfd8d9e0bbd29b063a7650c399a
Arya Shukla <arya.shukla.min17@itbhu.ac.in>
2021-06-13 00:34:12 +0530

    Update Main_Code.py

--- Main_Code.py
+++ Main_Code.py
@@ -14,12 +14,12 @@ neg_img=os.listdir(path_neg)
 #Create bg file for positive images
 for img in pos_img:
     line=path_pos+img+' '+'1 0 0 25 25'+'\n'
-    with open('/home/aryashukla/Desktop/Project/Cascade/bg_pos5873.lst', 'a') as f:
+    with open('/home/aryashukla/Desktop/Project/Cascade/bg_pos.lst', 'a') as f:
         f.write(line)
 #Create bg file for negative images
 for img in neg_img:
     line=path_neg+img+'\n'
-    with open('/home/aryashukla/Desktop/Project/Cascade/bg_neg5758.txt', 'a') as f:
+    with open('/home/aryashukla/Desktop/Project/Cascade/bg_neg.txt', 'a') as f:
         f.write(line)
 
 
@@ -38,7 +38,7 @@ sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavform
 sudo apt-get install libopencv-dev
 
 #Creating positive vector files
-opencv_createsamples -info bg_posmid_1619.lst -num 1619 -w 50 -h 50 -vec positives_1619.vec
+opencv_createsamples -info bg_pos.lst -num 5540 -w 25 -h 25 -vec positives_5540.vec
 
 #Training Cascade
-opencv_traincascade -data data -vec positives_1316.vec -bg bg_negmid_1600.txt -numPos 1100 -numNeg 550 -numStages 4 -w 50 -h 50
+opencv_traincascade -data data -vec positives_1316.vec -bg bg_negmid_1600.txt -numPos 5000 -numNeg 2700 -numStages 4 -w 25 -h 25
